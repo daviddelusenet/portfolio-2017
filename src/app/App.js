eaf631c470279749a12f1ae20d88625f37e2c178
David de Lusenet <lusenet@momkai.com>
2017-10-18 17:27:48 +0200

    Added very first React setup

---
+++ src/app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import update from 'immutability-helper';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // Bind functions
+
+    // Set initial state
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <p>hello world</p>
+    );
+  }
+
+}
+
+export default App;
